da1ea6b199538d51c77e977a651954ecfeb88a50
farah ashraf <97908376+farahaashraf@users.noreply.github.com>
2025-06-11 11:25:59 +0300

    removed-notf

--- qui-ai/backend/apps/notifications_project/notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NotificationListCreateView, NotificationDetailView
-
-urlpatterns = [
-    path('notifications/', NotificationListCreateView.as_view(), name='notification-list'),
-    path('notifications/<int:pk>/', NotificationDetailView.as_view(), name='notification-detail'),
-]
