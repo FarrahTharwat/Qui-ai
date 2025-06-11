ab3866620672f23ec28873a166ed169ed78906ec
fatmaa8 <fatmaabdelaziz308@gmail.com>
2025-06-11 21:30:51 +0300

    update
    deleted comments

--- qui-ai/backend/apps/track_progress/appwrite_client.py
+++ qui-ai/backend/apps/track_progress/appwrite_client.py
@@ -1,15 +1,6 @@
 import os
 os.environ["CURL_CA_BUNDLE"] = ""
 
-
-
-# client = Client()
-# client.set_endpoint(os.getenv("APPWRITE_ENDPOINT"))
-# client.set_project(os.getenv("APPWRITE_PROJECT_ID"))
-# client.set_key(os.getenv("APPWRITE_API_KEY"))
-
-# databases = Databases(client)
-
 from appwrite.client import Client
 from appwrite.services.databases import Databases
 import os
